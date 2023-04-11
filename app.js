0231cd974106801f7f81aac3704dfc866a9a2a1d
hugo <schramhugo77pgmail.com>
2023-04-11 19:02:12 +0200

    basic user login register

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "http://localhost:8081",
 };
 
 app.use(cors(corsOptions));
@@ -15,11 +17,22 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
+// To support URL-encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// To parse cookies from the HTTP Request
+app.use(cookieParser());
+
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "hello World!" });
 });
 
+app.get("/home", (req, res) => {
+  res.json({ message: "Home page" });
+});
+
+require("./app/routes/userRoute")(app);
 require("./app/routes/openaiRoute")(app);
 
 // set port, listen for requests
