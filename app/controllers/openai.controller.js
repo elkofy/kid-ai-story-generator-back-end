f61a7805d46214fb2b12d52f666c8632f63245fd
mathieu.thiolat <mathieu.thiolat@gmail.com>
2023-04-13 15:07:21 +0200

    adding all call to openai to generate add and redo

--- app/controllers/openai.controller.js
+++ app/controllers/openai.controller.js
@@ -9,7 +9,10 @@ const User = db.user;
 const Story = db.story;
 const Chapter = db.chapter;
 
+const modelEngine = 'text-davinci-003'; // or any other model you prefer
 
+var genre;
+var style;
 // generate story with text en picture
 exports.newStory = async (req, res) => {
   if (!req.body) {
@@ -29,26 +32,56 @@ exports.newStory = async (req, res) => {
   }
 
   const story = req.body.story;
-  const genre = req.body.genre;
-  const style = req.body.style;
+  genre = req.body.genre;
+  style = req.body.style;
   const characters = req.body.characters;
 
-  promptText = `Rédige moi un paragraphe d'une histoire avec information suivante : 
+  promptText = `Rédige une histoire courte avec information suivante : 
   Sujet : ${story}
   Genre d'histoire : ${genre}
   Personnage : [${characters}]
-  Si il s'agit du premier paragraphe il me faut le titre, et si le champ personnage est null il faudras que tu les crées toi même.
-  T'a réponse devras être formaté avec le model suivant : {"title":””,"paragraph":””}
-  Tu devras générer un paragaphe à la fois mais tu peut en rajouter à l”objet.`;
+  Il faut un titre, si le champ personnage est vide il faudras que tu les crées toi même.
+  T'a réponse devras être formaté en JSON avec le model suivant : {"title":"","paragraph":""}
+  Quoi qu'il arrive j'ai besoin que tu referme l'object json et que tu finise avec un point.`;
 
   promptImage = `Fait moi une image pour un livre de genre ${genre}, avec comme sujet :${story} et comme personnage : [${characters}], avec un style d'image ${style}`;
 
-  const result = await generateStory(promptText, promptImage)
+  try {
+    const textJson = await generateParagraph(promptText)
+    const url_image = await generateImage(textJson.title)
+
+    /*const textJson = {
+      title: "MOCK title",
+      paragraph: "MOCK paragraph"
+    };
+    const url_image = "MOCK url_image";*/
+      
+    const returnBody = await {
+      title: textJson.title,
+      story : [{
+        paragraph : textJson.paragraph+".",
+        image: url_image,
+      }]
+    };  
     //save chapter in BDD
-    saveStoryForUser(req.userId, result);
-    
+    saveStoryForUser(req.userId, returnBody);
     //return data to frontend
-    res.status(200).json(result);
+    res.status(200).json(returnBody);
+  
+  }catch(error){
+    if (error.response) {
+      //console.error(error.response.status, error.response.data);
+      return error.response.data;
+    } else {
+      console.error(`Error with OpenAI API request: ${error}`);
+      return {
+        error: {
+          message: 'An error occurred during your request.',
+        }
+      };
+    }
+  }
+  
 };
 
 // generate story with text en picture
@@ -59,13 +92,11 @@ exports.continueStory = async (req, res) => {
     });
     return;
   }*/
-  const story = req.body.story;
+  console.log("adding paragraphe")
+  const story = req.body.paragraph  ;
 
-  prompt = `Rédige la suite de l'histoire avec les même personnages
-  T'a réponse devras être formaté avec le model suivant : {"paragraph":””}
-  Tu devras générer un paragaphe à la fois.`;
+  prompt = `Rédige un paragraphe ${genre} court en relation avec l'histoire suivante `;
 
-  promptImage = `Fait moi une image pour : ${story}`;
 
   if (!configuration.apiKey) {
     res.status(500).json({
@@ -75,19 +106,108 @@ exports.continueStory = async (req, res) => {
     });
     return;
   }
-  const result = await generateStory(prompt,promptImage)
-  res.status(200).json(result);
+
+  try {
+    console.log(req.body)
+    console.log("Start generation paragraphe")
+
+    const textJson = await singleParagraph(prompt,story,2)
+    promptImage = `Fait moi une image style ${style},${genre} sujet :${textJson.paragraph}`;
+    console.log("Start generation image")
+    const url_image = await generateImage(promptImage)
+
+    /*const textJson = {
+      title: "MOCK title",
+      paragraph: "MOCK paragraph"
+    };
+    const url_image = "MOCK url_image";*/
+      
+    const returnBody = await {
+      story : [{
+        paragraph : textJson.paragraph,
+        image: url_image,
+      }]
+    };  
+    //const result = await generateStory(prompt,promptImage)
+    res.status(200).json(returnBody);
+
+  }catch(error){
+    if (error.response) {
+      //console.error(error.response.status, error.response.data);
+      return error.response.data;
+    } else {
+      console.error(`${error.message}`);
+      return {
+        error: {
+          message: 'An error occurred during your request.',
+        }
+      };
+    }
+  }
+  
   //continue a story
 };
 
 // generate story with text en picture
-exports.remakeLastParagraph = (req, res) => {
-  if (!req.body.title) {
+exports.remakeLastParagraph = async (req, res) => {
+  if (!req.body.paragraph) {
     res.status(400).send({
       message: "Content can not be empty!",
     });
     return;
   }
+  console.log("redo paragraphe")
+  const story = req.body.paragraph  ;
+
+  prompt = `Je n'ai pas, fait moi un meilleur paragraphe pour le style ${genre} remplace celui la `;
+
+
+  if (!configuration.apiKey) {
+    res.status(500).json({
+      error: {
+        message: "OpenAI API key not configured, please follow instructions in README.md",
+      }
+    });
+    return;
+  }
+
+  try {
+    console.log(req.body)
+    console.log("Start generation paragraphe")
+
+    const textJson = await singleParagraph(prompt,story,2)
+    promptImage = `Fait moi une image style ${style}, ${genre} sujet :${textJson.paragraph}`;
+    console.log("Start generation image")
+    const url_image = await generateImage(promptImage)
+
+    /*const textJson = {
+      title: "MOCK title",
+      paragraph: "MOCK paragraph"
+    };
+    const url_image = "MOCK url_image";*/
+      
+    const returnBody = await {
+      story : [{
+        paragraph : textJson.paragraph,
+        image: url_image,
+      }]
+    };  
+    //const result = await generateStory(prompt,promptImage)
+    res.status(200).json(returnBody);
+
+  }catch(error){
+    if (error.response) {
+      //console.error(error.response.status, error.response.data);
+      return error.response.data;
+    } else {
+      console.error(`${error.message}`);
+      return {
+        error: {
+          message: 'An error occurred during your request.',
+        }
+      };
+    }
+  }  
   //remake the last paragraph
 };
 
@@ -102,59 +222,44 @@ async function generateImage(prompt){
 }
 
 async function generateParagraph(promptText){
-  return openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [{role: "user", content: promptText}],
-    max_tokens: 200,  
+  return openai.createCompletion({
+    model: modelEngine,
+    prompt: promptText,
+    max_tokens: 200,
+    n: 1,
+    stop: null,
     temperature: 0.7,
-    stop: ["#end#"],
   }).then(async (response) => {
-  /*return openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: prompt,
-    temperature: 0.7,
-    max_tokens: 1000, 
-  }).then(async (response) => {*/
-    //res.send(response.data.choices[0]);
-    return (JSON.parse(response.data.choices[0].message.content.trim()));
+    var text = response.data.choices[0].text.trim()
+    console.log(text)
+    if(!text.endsWith("}"))
+      text+= "\"}"
+      
+    return (JSON.parse(text));
   });  
 }
 
-async function generateStory(promptText, promptImage) {
-  try {
-    //const url_image = await generateImage(promptImage)
-
-    //const textJson = await generateParagraph(promptText)
-
-    const textJson = {
-      title: "MOCK title",
-      paragraph: "MOCK paragraph"
-    };
-    const url_image = "MOCK url_image";
-
-    const returnBody = await {
-      title: textJson.title,
-      story : [{
-        paragraph : textJson.paragraph,
-        image: url_image,
-      }]
-    };
-
-    return returnBody;
-    
-  }catch(error){
-    if (error.response) {
-      //console.error(error.response.status, error.response.data);
-      return error.response.data;
-    } else {
-      console.error(`Error with OpenAI API request: ${error.message}`);
-      return {
-        error: {
-          message: 'An error occurred during your request.',
-        }
-      };
+async function singleParagraph(promptText,context){
+  const updatedContext = `${promptText} : ${context}`;
+  //console.log("continue prompt : "+updatedContext)
+  return openai.createCompletion({
+    model: modelEngine,
+    prompt: updatedContext,
+    max_tokens: 200,
+    n: 1,
+    stop: null,
+    temperature: 0.7,
+  }).then(async (response) => { 
+    var text = `{"paragraph":"${response.data.choices[0].text.trim()}"}`
+    console.log(text)
+    if(!text.endsWith("}")){
+      console.log("ici")
+      text+= '"}'
+      console.log(text)
     }
-  }
+      
+    return (JSON.parse(text));
+  });  
 }
 
 async function saveStoryForUser(currentUserId, data){
